50cea11e464d70827a7a3a43ad33edae0087f5e1
stinkyfingers <john_shenk@hotmail.com>
2022-04-20 14:01:45 -0500

    fmt terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # vars
 variable "region" {
-  type = string
+  type    = string
   default = "us-west-1"
 }
 
@@ -16,7 +16,7 @@ terraform {
 
 provider "aws" {
   profile = "jds"
-  region     = var.region
+  region  = var.region
 }
 
 # import
@@ -36,7 +36,7 @@ resource "aws_lambda_permission" "badlibs_server" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.badlibs_server.arn
   principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn    =  aws_lb_target_group.badlibs.arn
+  source_arn    = aws_lb_target_group.badlibs.arn
 }
 
 resource "aws_lambda_permission" "badlibs_server_live" {
@@ -44,7 +44,7 @@ resource "aws_lambda_permission" "badlibs_server_live" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_alias.badlibs_server_live.arn
   principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn = aws_lb_target_group.badlibs.arn
+  source_arn    = aws_lb_target_group.badlibs.arn
 }
 
 resource "aws_lambda_alias" "badlibs_server_live" {
@@ -70,7 +70,7 @@ resource "aws_lambda_function" "badlibs_server" {
 
 # IAM
 resource "aws_iam_role" "lambda_role" {
-  name = "badlibs-lambda-role"
+  name               = "badlibs-lambda-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -94,9 +94,9 @@ resource "aws_iam_role_policy_attachment" "cloudwatch-attach" {
 }
 
 resource "aws_iam_policy" "s3-policy" {
-  name = "badlibs-lambda-s3-policy"
+  name        = "badlibs-lambda-s3-policy"
   description = "Grants lambda access to s3"
-  policy = <<EOF
+  policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -124,14 +124,14 @@ resource "aws_lb_target_group" "badlibs" {
 }
 
 resource "aws_lb_target_group_attachment" "badlibs_server" {
-  target_group_arn  = aws_lb_target_group.badlibs.arn
-  target_id         = aws_lambda_alias.badlibs_server_live.arn
-  depends_on        = [aws_lambda_permission.badlibs_server_live]
+  target_group_arn = aws_lb_target_group.badlibs.arn
+  target_id        = aws_lambda_alias.badlibs_server_live.arn
+  depends_on       = [aws_lambda_permission.badlibs_server_live]
 }
 
 resource "aws_lb_listener_rule" "badlibs_server" {
-listener_arn = data.terraform_remote_state.stinkyfingers.outputs.stinkyfingers_https_listener
-priority = 2
+  listener_arn = data.terraform_remote_state.stinkyfingers.outputs.stinkyfingers_https_listener
+  priority     = 2
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.badlibs.arn
@@ -173,9 +173,9 @@ EOF
 # backend
 terraform {
   backend "s3" {
-    bucket = "remotebackend"
-    key    = "badlibs/terraform.tfstate"
-    region = "us-west-1"
+    bucket  = "remotebackend"
+    key     = "badlibs/terraform.tfstate"
+    region  = "us-west-1"
     profile = "jds"
   }
 }
